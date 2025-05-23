91eaf2930eabf45fd889688a987221c2dd1bff9a
edsoncepedi <edson.alves@cepedi.org.br>
2025-05-23 11:59:14 -0300

    correcao

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def button_calback(channel):
     print("Botão Pressionado")
     # Defina a URL e os dados a serem enviados na requisição POST
     url = "http://172.16.10.175/comando"
-    payload = {'comando': 'imprime_produto',}
+    payload = {'comando': 'imprime_produto'}
 
     # Cabeçalhos da requisição
     headers = {'Content-Type': 'application/json'}
@@ -18,14 +18,18 @@ def button_calback(channel):
     print(f"Código de Status: {response.status_code}")
     print(f"Conteúdo da Resposta: {response.text}")
 
+def main():
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.add_event_detect(10, GPIO.RISING, callback=button_calback, bouncetime=200)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(10, GPIO.RISING, callback=button_calback, bouncetime=200)
+    while True:
+        try:
+            time.sleep(1)
+        except:
+            GPIO.cleanup()
+            
 
-try:
-	while True:
-		time.sleep(1)
-except:
-	GPIO.cleanup()
+if __name__ == "__main__":
+    main()
